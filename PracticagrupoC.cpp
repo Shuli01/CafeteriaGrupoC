b804dd07ba38dfa69fc2fb7b9f86ab9b2e76c56d
isadam12 <101364283+isadam12@users.noreply.github.com>
2022-03-11 13:10:34 -0500

    Update PracticagrupoC.cpp

--- PracticagrupoC.cpp
+++ PracticagrupoC.cpp
@@ -1,15 +1,1 @@
-#include<iostream>
-using namespace std;
-int main(int argc, char** argv)
-{
-	cout << "Bienvenido a la cafeteria C" << endl;
-	
-	cout << "Para entrar a la cafeteria oprima intro" << endl;
-	
-	cin.get() 
-		
-    ;int puesto=1;
-    cout<<"Tu numero de mesa es "<<puesto;
-    cout<<endl;
-    return 0;
-}
+
